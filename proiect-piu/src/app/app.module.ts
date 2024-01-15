686421995336b3ba61955df602fa421b6190456d
Cosmin Varvari <cosmin.varvari@yahoo.com>
2024-01-15 22:06:18 +0200

    added weather display and journey schedule

--- proiect-piu/src/app/app.module.ts
+++ proiect-piu/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MyJourneysComponent } from './components/my-journeys/my-journeys.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { MyJourneysComponent } from './components/my-journeys/my-journeys.compon
     MatInputModule,
     MatAutocompleteModule,
     ReactiveFormsModule,
-    AsyncPipe
+    AsyncPipe,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proiect-piu/src/app/components/my-journeys/my-journeys.component.ts
+++ proiect-piu/src/app/components/my-journeys/my-journeys.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { Journey } from 'src/app/_models/journey';
 
 @Component({
   selector: 'app-my-journeys',
   templateUrl: './my-journeys.component.html',
   styleUrls: ['./my-journeys.component.scss']
 })
-export class MyJourneysComponent {
+export class MyJourneysComponent implements OnInit{
+  journey: Journey | undefined;
+  startingDate: Date | undefined;
+  endDate: Date | undefined;
+  todo: string[] = [];
+  done: string[] = [];
+  activity: string | undefined;
 
+  constructor() {} 
+
+  ngOnInit() {
+    this.journey = JSON.parse(localStorage.getItem("journey")!);
+    let localTodo = JSON.parse(localStorage.getItem("todo")!);
+    let localDone = JSON.parse(localStorage.getItem("done")!);
+    if(localTodo !== null && localTodo !== undefined) {
+      this.todo = localTodo;
+    }
+    if(localDone !== null && localDone !== undefined) {
+      this.done = localDone;
+    }
+    console.log(this.journey);
+    this.startingDate = this.randomDate(new Date(2024, 0, 1), new Date(2024, 0, 15));
+    this.endDate = this.randomDate(new Date(2024, 0, 16), new Date(2024, 0, 30));
+  }
+
+  randomDate(start: Date, end: Date) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  numSequence(n: number): Array<number> { 
+    return Array(n); 
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  addToDoList(){
+    this.todo.push(this.activity!);
+    localStorage.setItem("todo", JSON.stringify(this.todo));
+    localStorage.setItem("done", JSON.stringify(this.done));
+  }
 }
